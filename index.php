98c32aac268d31c0347fe1ea9143f1c1fdfdcbe9
fikrii8 <148346003+fikrii8@users.noreply.github.com>
2024-10-30 12:10:15 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+$pilihan = 3;
+
+$angka1 = -3;
+$angka2 = -1;
+$penjumlahan = $angka1 + $angka2;
+
+if ($pilihan == 1){
+    if ($angka1 > 0 && $angka2 > 0){
+        echo "hasil penjumlahan = " . $angka1 + $angka2;
+    }
+    else  {
+        echo "Angkanya harus positif";
+    }
+}   elseif ($pilihan == 2) {
+    if ($angka1 > 0 && $angka2 > 0){
+        echo "hasil rata rata = " . $penjumlahan/2;
+    }
+    else {
+        echo "Angkanya harus positif";
+    }
+}   elseif ($pilihan == 3) {
+    if ($angka1 > 0 && $angka2 > 0){
+        
+        echo "hasil penjumlahan = " . $angka1 + $angka2 . "hasil rata rata = " . $penjumlahan/2;
+    }
+    else {
+        echo "Angkanya harus positif";
+    }
+}
+
+?>
